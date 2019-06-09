af4ccbbb1b6ec07d972a71a9562aed218de2a634
UncleKo <info@shrewd.work>
2019-06-10 03:03:56 +0900

    Login and Logout

--- django_project/urls.py
+++ django_project/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users import views as user_views
 # from users.views import register
@@ -6,6 +7,9 @@ from users import views as user_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
+    path('profile/', user_views.profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     # path('register/', register, name='register'),
     path('', include('blog.urls')),
 ]
