ef4836e21b7d143f3eda161d8ae3810c5bd9b59c
UncleKo <info@shrewd.work>
2019-06-22 23:07:27 +0900

    Image Upload to Post

--- django_project/settings.py
+++ django_project/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'users.apps.UsersConfig',
     'crispy_forms',
+    'imagekit',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
