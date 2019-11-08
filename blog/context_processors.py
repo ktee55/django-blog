a5fe32661d17493a4b6fc995ef8644443914e4d7
ko <ktee55@gmail.com>
2019-11-09 03:16:25 +0900

    Latest Posts and Drafts on Sidebar

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -5,6 +5,8 @@ def global_val(request):
   archives = Post.objects.values('date_posted__year','date_posted__month').annotate(Count('id')).order_by()
 
   return {
+    'latest_posts': Post.objects.all().order_by('-date_posted')[:5],
+    'drafts': Post.objects.filter(draft=True).order_by('-date_posted')[:5],
     'categories': Category.objects.all(),
     'tags': Tag.objects.all(),
     'archives': archives
