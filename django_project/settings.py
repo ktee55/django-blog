7ccc2e2d31e15e4cb6e6144fe4bed7624b11d001
ko <ktee55@gmail.com>
2020-07-26 15:51:52 +0900

    Recovered Storage and AWS settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'photos.apps.PhotosConfig',
     'crispy_forms',
     'imagekit',
-    # 'storages',
+    'storages',
     'rest_framework',
     # 'rest_framework.authtoken',  
     # 'allauth', 
@@ -164,19 +164,19 @@ LOGIN_URL = 'login'
 # CSRF_COOKIE_HTTPONLY = True
 
 
-# AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-# AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-# AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
-# AWS_S3_FILE_OVERWRITE = False
-# AWS_DEFAULT_ACL = None
-# # https を有効にします
-# # AWS_S3_SECURE_URLS = True
-# # 認証クエリーを無効にします
-# AWS_QUERYSTRING_AUTH = False
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+# https を有効にします
+AWS_S3_SECURE_URLS = True
+# 認証クエリーを無効にします
+AWS_QUERYSTRING_AUTH = False
 
 # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-# DEFAULT_FILE_STORAGE = 'blog.storages.CustomS3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'blog.storages.CustomS3Boto3Storage'
 
 
 REST_FRAMEWORK = {
